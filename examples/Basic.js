59f5cae61aa96774d7e45689b0ae408cb86dad23
Pranay S <pranay.sahota@practo.com>
2019-06-26 12:06:56 +0530

    Initial commit, perf test w/ 300 fields

--- examples/Basic.js
+++ examples/Basic.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
 import { Debug } from './Debug';
+const getFields = (i) => {
+  return (
+    <React.Fragment>
+    <label htmlFor={`${i}firstName`}>First Name</label>
+    <Field name={`${i}firstName`} placeholder="Jane" />
+`
+    <label htmlFor={`${i}lastName`}>Last Name</label>
+    <Field name={`${i}lastName`} placeholder="Doe" />
 
+    <label htmlFor={`${i}email`}>Email</label>
+    <Field name={`${i}email`} placeholder="jane@acme.com" type="email" />
+    </React.Fragment>
+  );
+}
+const getMultipleFileds = () => {
+  const toReturn = [];
+  for(let i=0;i<100; i+=1){
+    toReturn.push(getFields(i));
+  }
+  return toReturn;
+}
 const Basic = () => (
   <div>
     <h1>Sign Up</h1>
@@ -18,16 +38,8 @@ const Basic = () => (
       }}
       render={() => (
         <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field name="firstName" placeholder="Jane" />
-
-          <label htmlFor="lastName">Last Name</label>
-          <Field name="lastName" placeholder="Doe" />
-
-          <label htmlFor="email">Email</label>
-          <Field name="email" placeholder="jane@acme.com" type="email" />
-          <button type="submit">Submit</button>
-          <Debug />
+         {getMultipleFileds()}
+         <button type="submit">Submit</button>
         </Form>
       )}
     />
